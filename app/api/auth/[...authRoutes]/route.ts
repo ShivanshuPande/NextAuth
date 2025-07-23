76997b155c2a3106553bca4ad20bcfd5c792a0e0
Shivanshu Pandey <pandeyshivanshu2005@gmail.com>
2025-07-24 00:00:28 +0530

    few changes in the auth route

--- app/api/auth/[...authRoutes]/route.ts
+++ app/api/auth/[...authRoutes]/route.ts
@@ -1,19 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(
-    req: NextRequest, 
-    { params }: {
-        params: Promise<{
-            authRoutes: string[]
-        }>
-    }
-) {
-    const { authRoutes } = await params;
-    
+export async function GET (req : NextRequest , {params} : {
+    params : Promise <{
+        authRoutes : string[]
+    }>
+}){
+    const {authRoutes} =  await params ;
     console.log(authRoutes);
-    
     return NextResponse.json({
-        message: "You have visited the auth route", 
-        root: authRoutes
-    });
+        message :"this the route server"
+    })
 }
+
+// import { NextRequest, NextResponse } from "next/server";
+
+// export async function GET(
+//     req: NextRequest, 
+//     { params }: {
+//         params: Promise<{
+//             authRoutes: string[]
+//         }>
+//     }
+// ) {
+//     const { authRoutes } = await params;
+    
+//     console.log(authRoutes);
+    
+//     return NextResponse.json({
+//         message: "You have visited the auth route", 
+//         root: authRoutes
+//     });
+// }
